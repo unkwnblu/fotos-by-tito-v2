91e5ea4e9cb7c0939d6c0a9eaf351a7bae424e64
unkwnblu <dagbale16@gmail.com>
2026-02-05 20:21:51 +0100

    feat: implement root layout with metadata, fonts, theme provider, toaster, and Google Tag Manager integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,13 +32,15 @@ export const metadata: Metadata = {
         alt: "FotosByTito Logo",
       },
     ],
-    locale: "en_US",
+    locale: "en_NL",
     type: "website",
   },
 };
 
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -47,9 +49,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NCCSX2HH');
+        `}
+      </Script>
+      {/* End Google Tag Manager */}
       <body
         className={` ${robotoFlex.variable} ${nunito.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NCCSX2HH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
